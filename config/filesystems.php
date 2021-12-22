f2f8859f9a53d8f4371410583fce668af0e21c2b
RainelU <urainel@gmail.com>
2021-12-21 23:01:51 -0300

    git push rurdaneta

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,12 +43,12 @@ return [
         ],
         'uploads' => [
             'driver' => 'local',
-            'root' => "C:/Users/migue/Downloads/madecms/public",
+            'root' => 'C:\xampp\htdocs\madecms\public\uploads',
             'visibility' => 'public',
         ],
         'uploads_user' => [
             'driver' => 'local',
-            'root' => "C:/Users/migue/Downloads/madecms/public",
+            'root' => 'C:\xampp\htdocs\madecms\public\uploads',
         ],
 
         's3' => [
